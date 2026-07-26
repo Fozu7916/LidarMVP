63ff9f202edc3c4ea2086941885fb4be021209d3
Fozu <132448722+Fozu7916@users.noreply.github.com>
2026-07-26 18:16:14 +0700

    Change gitignore

--- obj/Debug/net10.0/MyProjectName.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyProjectName.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProjectName")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05be5b830d2f5e2bedd59daada96af4d69d1ddb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bdf58fcdf46a1c7f9a31119c51f190fbd726d22")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProjectName")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProjectName")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
